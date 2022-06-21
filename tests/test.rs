b407b7e284251a53fa145505a72d0ca3b0251b38
Christoph Siedentop <commits@siedentop.name>
2022-06-20 18:45:09 -0700

    Format tests

--- tests/test.rs
+++ tests/test.rs
@@ -6,10 +6,15 @@ fn _git_quickfix_binary() -> Command {
 }
 
 // Require that
-// 1) There shall be afterwards a branch. The commits should be going backwards: (1) the content is the same as the cherry-pick commit. (2) the next commit is the same as origin/main
-// 2) The original quickfix commit should not be present on the original branch. (and opposite if --keep is provided)
+// 1) There shall be afterwards a branch. The commits should be going backwards:
+//      (1) the content is the same as the cherry-pick commit.
+//      (2) the next commit is the same as origin/main
+// 2) The original quickfix commit should not be present on the original branch.
+//    (and opposite if --keep is provided)
 // 3) Afterwards making a commit should be possible.
-// 4) Afterwards, making another commit and repeating the exercise should also work. (It might not because of "cannot create a tree from a not fully merged index.; class=Index (10); code=Unmerged (-10)")
+// 4) Afterwards, making another commit and repeating the exercise should also
+//    work. (It might not because of "cannot create a tree from a not fully merged
+//    index.; class=Index (10); code=Unmerged (-10)")
 
 // Test from a subdirectory.
 
@@ -17,5 +22,11 @@ fn _git_quickfix_binary() -> Command {
 
 // Test with untracked changes present.
 
-// Test that the stash-list is not touched afterwards if the --stash option is used, and if it is not used.
-// Test that stashing and a subsequent error leaves the repo untouched (i.e. the stash is popped).
+// Test that the stash-list is not touched afterwards if the --stash option is
+// used, and if it is not used.
+// Test that stashing and a subsequent error leaves the repo untouched (i.e. the
+// stash is popped).
+
+// https://github.com/siedentop/git-quickfix/issues/7
+// A commit made on main should be moved away from main, and no other commit shall
+// be made on main.
